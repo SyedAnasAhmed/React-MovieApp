04c64944f6557b279eb9ca7275564a8dcbe8359a
Syed Anas Ahmed <syedanasahmed412@gmail.com>
2024-04-17 04:05:14 +0500

    Data rendered on ui

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Movies from './Components/Movies'
+import { Navbar } from './Components/Navbar'
+
+
+function App() {
+  return (
+    <>
+      {/* <Navbar /> */}
+      <Movies/>
+    </>
+  )
+}
+
+export default App
+
+
---
+++ src/Components/Movies.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import { CardOne } from './Card'
+import { Navbar } from './Navbar'
+
+function Movies() {
+
+    const [movielist, setmovielist] = useState([])
+
+    const getmovie = () => {
+        fetch("https://api.themoviedb.org/3/discover/movie?api_key=d0741b19756585f8d7dfb3265b9baa80")
+            .then(res => res.json())
+            .then(json => setmovielist(json.results))
+            .catch(error => console.log(error.code))
+    }
+
+    console.log(movielist)
+
+    useEffect(() => {
+        getmovie()
+    }, [])
+
+    return (
+        <>
+
+        <div className="flex flex-wrap justify-center gap-4 bg-black  ">
+        {
+            movielist.map((movie) => (
+                <CardOne key={movie.id} image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+                            heading={movie.title}
+                            desc={movie.overview}
+                />
+            ))
+        }
+        </div>
+        </>
+    );
+}
+
+export default Movies
